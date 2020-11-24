fbada2e854f184edf309dae50b515dd31dff93a8
Jihad Noureddine <jihad.nd@gmail.com>
2020-11-24 17:38:53 +0200

    the primary showroom structure

---
+++ src/containers/landing_page/landingpage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Classes from "./landingpage.module.css"; // when you import the css this you prevent the css from being inherited by the children
+
+export default class landingpage extends Component {
+    render() {
+        return (
+            <div className={Classes.Label}>
+                
+            </div>
+        )
+    }
+}
---
+++ src/containers/showroom/showroom.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class showroom extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    } 
+}
